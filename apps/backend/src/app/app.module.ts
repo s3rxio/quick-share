5a7e4646dcb3cb39e4d900ee02d71fcd734f286b
s3rxio <cergotabagua222@gmail.com>
2024-07-10 12:43:49 +0300

    feat(backend): add database module

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
+import { DbModule } from "./db/db.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.${process.env.NODE_ENV}`, `.env`, `.env.local`]
-    })
+    }),
+    DbModule
   ],
   controllers: [AppController],
   providers: [AppService]
