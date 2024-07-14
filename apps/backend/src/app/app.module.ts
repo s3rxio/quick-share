d380d32f65e71ad91a73158c50142e0a9e444194
s3rxio <cergotabagua222@gmail.com>
2024-07-14 15:43:52 +0300

    refactor(backend): move typeorm config to app module

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { Module } from "@nestjs/common";
-
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
-import { DbModule } from "./db/db.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./user/user.entity";
+import { UserModule } from "./user/user.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.${process.env.NODE_ENV}`, `.env`, `.env.local`]
     }),
-    DbModule
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => ({
+        type: "postgres",
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [],
+        synchronize: true
+      })
+    })
   ],
   controllers: [AppController],
   providers: [AppService]
