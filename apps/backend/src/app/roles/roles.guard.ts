4e6bc73c60ac3c4c02dcb9b7c2a0c5c4c863769b
s3rxio <cergotabagua222@gmail.com>
2024-07-16 00:09:06 +0300

    refactor(backend): remove console log in roles guard

--- apps/backend/src/app/roles/roles.guard.ts
+++ apps/backend/src/app/roles/roles.guard.ts
@@ -17,8 +17,6 @@ export class RolesGuard implements CanActivate {
       return true;
     }
 
-    console.log(requiredRoles);
-
     const { user } = context.switchToHttp().getRequest();
     return requiredRoles.some(role => user.roles?.includes(role));
   }
